bc131f73f48957110808ba1b6244bfe72fb536d9
Vishal Singh <vishalsingh2376@gmail.com>
2021-06-29 12:30:32 +0530

    open braces fixed

--- main.cpp
+++ main.cpp
@@ -1,24 +1,88 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
 #include <string>
 
 using namespace std;
 
-void convert(string);
+// void json_tree(string);
+void json_tree();
 
 int main()
 {
-	string file_path;
-	cout << "Enter your file path : ";
-	cin >> file_path;
+	// string file_path;
+	// cout << "Enter your file path : ";
+	// cin >> file_path;
 
-	convert(file_path);
+	// json_tree(file_path);
+	json_tree();
+
 
 	return 0;
 }
 
-void convert(string file_path)
+// void json_tree(string file_path)
+void json_tree()
 {
-	fstream fin, fout;
+	cout << "Enter inside function";
+	ifstream fin;
+	ofstream fout;
+	int tabs, tokens; //here tokens are  { } , :
+	tokens = -1;
+	char ch;
+	cout << "open function";
+	fin.open("sample.json");
+	if (!fin)
+		cout << "Failed to open file";
+	else
+	{
+		cout << "open file";
+		fout.open("result.txt");
+		while (fin.get(ch))
+		{
+
+			if (ch == '{')
+			{
+				tokens++;
+				tabs = tokens;
+				while (tabs)
+				{
+					fout << "\t";
+					tabs--;
+				}
+
+				fout << ch << "\n";
+				tabs = tokens + 1;
+
+				while (tabs)
+				{
+					fout << "\t";
+					tabs--;
+				}
+			}
+			else if (ch == '}')
+			{
+				tabs = tokens;
+				while (tabs)
+				{
+					fout << "\t";
+					tabs--;
+				}
+
+				fout << ch << "\n";
+				tokens--;
+				tabs = tokens + 1;
+
+				while (tabs)
+				{
+					fout << "\t";
+					tabs--;
+				}
+			}
+			else
+				fout << ch;
+		}
+		fout.close();
+	}
+	fin.close();
+	cout << "Complete";
 }
